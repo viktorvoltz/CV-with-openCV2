260e571a0ff037f987b8bf3da8bdf2668de7eaa8
chinonso <vctrchinyeaka@gmail.com>
2020-01-31 13:13:19 +0100

    added cv2.py

---
+++ cv2.py
@@ -0,0 +1,26 @@
+import cv2
+
+cap =  cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20, (640, 480))
+
+while(True):
+    ret, frame = cap.read(50)
+    if ret == True:
+
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        out.write(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
